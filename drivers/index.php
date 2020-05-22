8ab06cca5410c4e9f128b16b4ec84e32241b1a8d
m1czi <internety23@gmail.com>
2020-05-22 03:11:51 +0200

    v3

---
+++ drivers/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Zadanie</title>
+    <meta charset="UTF-8" />
+    <link href="style.css" rel="stylesheet" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="item top">
+         <div class="header"> <a href="../index.php"> <img src="F1.png" height="100px"></div></a>
+      </div>
+      <div class="item left">
+       
+      </div>
+	  
+      <div class="item mid">
+		<div>
+			<p>Tu będzie tabela kierowców</p>
+		
+		</div>
+      </div>
+	  
+
+      <div class="item right">
+        <a>KIEROWCY</a>
+      
+    
+      </div>
+      <div class="leftv2">
+        <a class="text2" href="../teams/index.html">ZESPOLY</a> <br/>
+        
+      </div>
+      <div class="item left">
+        <a class="text2" href="../tracks/index.html">TORY</a> <br/>
+      </div>
+	  
+	</div>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Zadanie</title>
+    <meta charset="UTF-8" />
+    <link href="style.css" rel="stylesheet" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="item top">
+        <div class="header"> <a href="index.php"> <img src="F1.png"></div></a>
+       
+		
+      </div>
+	  
+	  <div class="road">
+	  
+	  
+	    <img src="bolid.png" class="bolid">
+		<button class="btn">JAZDA!</button>
+        <button class="btn2">STOP</button>
+	  </div>
+	  
+	  
+      <div class="item left">
+       
+      </div>
+	  
+      <div class="item mid">
+		<div>
+			<ul>
+			<li>Jest to strona poświęcona Formule 1</li>
+			<li>Dotyczy ona sezonu 2020</li>
+			
+			</ul>
+			Do następnego GP pozostało:
+			
+			<div
+			class="zegar" <p id="demo"></p>
+			</div>
+
+			<img src="driversall.jpg" height="410px">
+			
+				
+				<audio controls>
+					<source src="f1theme.mp3" type="audio/mpeg">
+				</audio>
+				
+				
+		</div>
+      </div>
+	  
+
+      <div class="item right">
+        <a class="text2" href="drivers/index.php">KIEROWCY</a><br>
+		
+      
+        </div>
+      <div class="leftv2">
+        <a class="text2" href="teams/index.php">ZESPOLY</a>
+        
+      </div>
+      <div class="item left">
+        <a class="text2" href="tracks/index.php">TORY</a>
+      </div>
+	  
+	</div>
+  </body>
+  <script src="main.js"></script>
+</html>
